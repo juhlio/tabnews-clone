c592ecbff38dfcc22732e49bc73b387b2ce03141
Júlio <julio@jcsr.com.br>
2023-10-14 20:19:51 -0300

    Adicionando os scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 function Home() {
-
-    return <div>
-       <h1>Oi Natalia</h1> 
-       <p>Só quero te lembrar que eu te amo</p> 
+  return (
+    <div>
+      <h1>Oi Natalia</h1>
+      <p>Só quero te lembrar que eu te amo</p>
     </div>
+  );
 }
 
 export default Home;
